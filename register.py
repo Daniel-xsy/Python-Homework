ccfb745892a99c6b9c37301b1e34c89d9da9507f
Daniel <1491387884@qq.com>
2020-11-14 22:30:53 +0800

    完成登陆界面绘制

--- register.py
+++ register.py
@@ -1,33 +1,33 @@
 import tkinter as tk
-import register
+import tkinter.messagebox as tkm
 
 window=tk.Tk()
 window.title=('学生成绩录入系统')
 window.geometry('450x300')
 
-var=tk.StringVar()
+#var=tk.StringVar()
 
-tk.Label(window,text='学生成绩查询系统',font=('华文行楷',20),width=30,height=2).place(x=40,y=10)
-tk.Label(window,text='用户名',width=30,height=2).place(x=15,y=120)
-tk.Label(window,text='密码',width=30,height=2).place(x=15,y=165)
+#文字
+tk.Label(window,text='学生成绩查询系统',font=('华文行楷',20),width=30,height=2).place(x=40,y=0)
+tk.Label(window,text='用户名',width=30,height=2).place(x=-5,y=130)
+tk.Label(window,text='密码',width=30,height=2).place(x=-5,y=175)
 
-#用户名输入
+#用户名输入 密码输入
 user_id=tk.Entry(window,show=None,font=('Arial',14))
-user_id.place(x=145,y=130)
-
-#密码输入
+user_id.place(x=130,y=140)
 user_password=tk.Entry(window,show='*',font=('Arial',14))
-user_password.place(x=145,y=170)
+user_password.place(x=130,y=180)
 
 #按钮
-tk.Button(window,text='注册',width=10,height=1,command=None).place(x=100,y=230)
+tk.Button(window,text='注册',width=10,height=1,command=None).place(x=70,y=230)
 tk.Button(window,text='登陆',width=10,height=1,command=None).place(x=190,y=230)
-tk.Button(window,text='退出',width=10,height=1,command=None).place(x=250,y=230)
+tk.Button(window,text='退出',width=10,height=1,command=None).place(x=310,y=230)
 
-canvas=tk.Canvas(window,height=100,width=94)
+#图片
+canvas=tk.Canvas(window,height=75,width=98)
 image_file=tk.PhotoImage(file='hust.gif')
-image=canvas.create_image(250,0,anchor='n',image=image_file)
-canvas.pack()
+image=canvas.create_image(50,-10,anchor='n',image=image_file)
+canvas.place(x=170,y=50)
 
 window.mainloop()
 '''
