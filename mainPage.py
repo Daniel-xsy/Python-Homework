8d9951fbc9a56ff451601c3ed9a9edbba815c848
Daniel <1491387884@qq.com>
2020-11-18 11:03:23 +0800

    主要功能实现

--- mainPage.py
+++ mainPage.py
@@ -13,6 +13,28 @@ class mainPage(object):
         else:
             self.creatTeacherPage()
     def creatStudentPage(self):
-        pass
+        frame=ttk.Frame(self.root)
+        frame.grid()
+        inquire_button=ttk.Button(frame,text='成绩查询',command=None)
+        inquire_button.grid()
+        frame.mainloop()
     def creatTeacherPage(self):
+        frame=ttk.Frame(self.root)
+        frame.grid()
+        inquire_button=ttk.Button(frame,text='学生成绩查询',command=None)
+        class_button=ttk.Button(frame,text='班级成绩查询',command=None)
+        input_button=ttk.Button(frame,text='成绩录入',command=None)
+        inquire_button.grid(row=1,column=1)
+        class_button.grid(row=2,column=1)
+        input_button.grid(row=2,column=1)
+        frame.mainloop()
+
+class studentPage(mainPage):
+    def __init__(self):
+        pass
+        
+class teacherPage(mainPage):
+    def __init__(self):
+        pass
+    def inputGrade(self):
         pass
