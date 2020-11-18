9a437bbb36057ea8976fc05277409b66fd975532
Daniel <1491387884@qq.com>
2020-11-18 21:36:20 +0800

    '主函数'

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 import login
 
 if __name__=="__main__":  
-    root=Tk()
+    root=tk.Tk()
     #添加标题
     root.title('Student Grade Enquiry System')
-    root.geometry('550x400')
+    screenwidth = root.winfo_screenwidth()
+    screenheight = root.winfo_screenheight()
+    width=360
+    height=300
+    #将窗口放置于屏幕中间
+    root.geometry('%dx%d+%d+%d'%(width, height,\
+         (screenwidth-width)/2, (screenheight-height)/2))
+    #进入登陆注册界面
     login.LoginPage(root)
     root.mainloop()
