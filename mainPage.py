244a4e81b39b79ea04adbe37878b77f9084d0a4d
Daniel <1491387884@qq.com>
2020-11-18 13:08:56 +0800

    '查询功能'

--- mainPage.py
+++ mainPage.py
@@ -6,12 +6,47 @@ import csv
 class mainPage(object):
     def __init__(self,master=None,id=None,user=None):
         self.root=master
+        self.infor_frame=None
         self.id=id
         self.user=user
+        self.search_input=StringVar()
         if user=='student':
             studentPage(self.root)
         else:
             teacherPage(self.root)
+    def searchPage(self):
+        search_lable=ttk.Label(self.root,text='请输入学生姓名或学号')
+        search_entry=ttk.Entry(self.root,textvariable=self.search_input)
+        search_button=ttk.Button(self.root,text='确认',command=self.searchGrade)
+        name_label=ttk.Label(self.root,text='姓名')
+        id_label=ttk.Label(self.root,text='学号')
+
+        search_lable.grid(row=1,column=1,columnspan=2,pady=5)
+        search_entry.grid(row=2,column=1,columnspan=2,padx=5)
+        search_button.grid(row=2,column=3)
+        name_label.grid(row=3,column=1,padx=2,pady=3)
+        id_label.grid(row=3,column=2,padx=2)
+    def searchGrade(self):
+        string=self.search_input.get()
+        with open('students.csv','r',encoding='utf-8') as file:
+            reader=csv.reader(file)
+            for row in reader:
+                if row[0]==string or row[1]==string:
+                    self.infor_frame=ttk.Frame(self.root)
+                    self.infor_frame.grid(row=4,column=1,rowspan=2,columnspan=4)
+                    name=str(row[1])
+                    id=str(row[0])
+                    grade=str(row[4:])
+                    name_label=ttk.Label(self.infor_frame,text=name)
+                    id_lable=ttk.Label(self.infor_frame,text=id)
+                    grade_lable=ttk.Label(self.infor_frame,text=grade)
+                    name_label.grid(row=1,column=2)
+                    id_lable.grid(row=1,column=1)
+                    grade_lable.grid(row=1,column=3,columnspan=3)
+                    return
+            tkm.showwarning(title='系统提示',message='未查找到任何信息')
+            return
+
 
 
 
@@ -30,40 +65,15 @@ class studentPage(mainPage):
 class teacherPage(mainPage):
     def __init__(self,master=None):
         self.root=master
+        self.search_input=StringVar()
         self.creatTeacherPage()
     def creatTeacherPage(self):
         frame=ttk.Frame(self.root)
         frame.grid()
-        inquire_button=ttk.Button(frame,text='学生成绩查询',command=None)
+        inquire_button=ttk.Button(frame,text='学生成绩查询',command=self.searchPage)
         class_button=ttk.Button(frame,text='班级成绩查询',command=None)
         input_button=ttk.Button(frame,text='成绩录入',command=None)
         inquire_button.grid(row=1,column=1)
         class_button.grid(row=2,column=1)
         input_button.grid(row=2,column=1)
         frame.mainloop()
-    def studentInforSearch(self):
-        search_input=StringVar()
-        search_lable=ttk.Label(self.root,text='请输入学生姓名或姓名')
-        search_entry=ttk.Entry(self.root,textvariable=search_input)
-        search_button=ttk.Button(self.root,text='确认')
-        name_label=ttk.Label(self.root,text='姓名')
-        id_label=ttk.Label(self.root,text='学号')
-
-        search_lable.grid(row=1,column=1,columnspan=2,pady=5)
-        search_entry.grid(row=2,column=1,columnspan=2,padx=5)
-        search_button.grid(row=2,column=3)
-        name_label.grid(row=3,column=1,padx=2,pady=3)
-        id_label.grid(row=3,column=2,padx=2)
-        infor=self.searchGrade(search_input.get())
-        if infor:
-            infor_label=ttk.Label(self.root,text=str(infor))
-            infor_label.grid(row=4,rowspan=4)
-        else:
-            tkm.showwarning(title='系统提示',message='未查找到任何信息')
-    def searchGrade(self,string):
-        with open('students.csv','r',encoding='utf-8') as file:
-            reader=csv.reader(file)
-            for row in reader:
-                if row[0]==string or row[1]==string:
-                    return row
-            return None
